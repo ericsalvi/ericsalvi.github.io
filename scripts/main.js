03b9eb2fdb8ac1133c7ac1a21d10abf93b26cd75
ericsalvi <ericsalvi@me.com>
2021-07-27 17:22:42 -0400

    Added local storage for the dark and light toggle

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,40 @@
-document.querySelector('[data-switch-dark]').addEventListener('click', function() {
-    document.body.classList.toggle('dark');
-  });
+// Inspired by Kevin Powell's https://www.youtube.com/watch?v=wodWDIdV9BY
+
+// check for saved 'darkMode' in localStorage
+let darkMode = localStorage.getItem('darkMode'); 
+
+const darkModeToggle = document.querySelector('[data-switch-dark]');
+
+const enableDarkMode = () => {
+  // 1. Add the class to the body
+  document.body.classList.add('dark');
+  // 2. Update darkMode in localStorage
+  localStorage.setItem('darkMode', 'enabled');
+}
+
+const disableDarkMode = () => {
+  // 1. Remove the class from the body
+  document.body.classList.remove('dark');
+  // 2. Update darkMode in localStorage 
+  localStorage.setItem('darkMode', null);
+}
+ 
+// If the user already visited and enabled darkMode
+// start things off with it on
+if (darkMode === 'enabled') {
+  enableDarkMode();
+}
+
+// When someone clicks the button
+darkModeToggle.addEventListener('click', () => {
+  // get their darkMode setting
+  darkMode = localStorage.getItem('darkMode'); 
+  
+  // if it not current enabled, enable it
+  if (darkMode !== 'enabled') {
+    enableDarkMode();
+  // if it has been enabled, turn it off  
+  } else {  
+    disableDarkMode(); 
+  }
+});
