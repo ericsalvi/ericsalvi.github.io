a119921e243544946b066cfd619de62cf7b1a9b7
ericsalvi <ericsalvi@me.com>
2021-11-12 16:36:09 -0500

    added interactive portfolio items

--- scripts/main.js
+++ scripts/main.js
@@ -1,40 +1,88 @@
 // Inspired by Kevin Powell's https://www.youtube.com/watch?v=wodWDIdV9BY
 
 // check for saved 'darkMode' in localStorage
-let darkMode = localStorage.getItem('darkMode'); 
+let darkMode = localStorage.getItem("darkMode");
 
-const darkModeToggle = document.querySelector('[data-switch-dark]');
+const darkModeToggle = document.querySelector("[data-switch-dark]");
 
 const enableDarkMode = () => {
   // 1. Add the class to the body
-  document.body.classList.add('dark');
+  document.body.classList.add("dark");
   // 2. Update darkMode in localStorage
-  localStorage.setItem('darkMode', 'enabled');
-}
+  localStorage.setItem("darkMode", "enabled");
+};
 
 const disableDarkMode = () => {
   // 1. Remove the class from the body
-  document.body.classList.remove('dark');
-  // 2. Update darkMode in localStorage 
-  localStorage.setItem('darkMode', null);
-}
- 
+  document.body.classList.remove("dark");
+  // 2. Update darkMode in localStorage
+  localStorage.setItem("darkMode", null);
+};
+
 // If the user already visited and enabled darkMode
 // start things off with it on
-if (darkMode === 'enabled') {
+if (darkMode === "enabled") {
   enableDarkMode();
 }
 
 // When someone clicks the button
-darkModeToggle.addEventListener('click', () => {
+darkModeToggle.addEventListener("click", () => {
   // get their darkMode setting
-  darkMode = localStorage.getItem('darkMode'); 
-  
+  darkMode = localStorage.getItem("darkMode");
+
   // if it not current enabled, enable it
-  if (darkMode !== 'enabled') {
+  if (darkMode !== "enabled") {
     enableDarkMode();
-  // if it has been enabled, turn it off  
-  } else {  
-    disableDarkMode(); 
+    // if it has been enabled, turn it off
+  } else {
+    disableDarkMode();
   }
 });
+
+filterSelection("all");
+function filterSelection(c) {
+  var x, i;
+  x = document.getElementsByClassName("portfolio__item");
+  if (c == "all") c = "";
+  // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
+  for (i = 0; i < x.length; i++) {
+    w3RemoveClass(x[i], "show");
+    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+  }
+}
+
+// Show filtered elements
+function w3AddClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) == -1) {
+      element.className += " " + arr2[i];
+    }
+  }
+}
+
+// Hide elements that are not selected
+function w3RemoveClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    while (arr1.indexOf(arr2[i]) > -1) {
+      arr1.splice(arr1.indexOf(arr2[i]), 1);
+    }
+  }
+  element.className = arr1.join(" ");
+}
+
+// Add active class to the current control button (highlight it)
+var btnContainer = document.getElementById("portfolio__filters");
+var btns = btnContainer.getElementsByClassName("filters");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function () {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
